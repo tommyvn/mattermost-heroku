a2e372e14e1546fe813dcc96eb97916ec4b4bed1
Tom van Neerijnen <tom@tomvn.com>
2015-10-23 15:24:00 +0100

    remove unnecessary environment handling, it's already in the env

--- start.sh
+++ start.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# set up envs
-
-export PORT=${PORT:=1234}
-export TEAM_SETTINGS__SITE_NAME=${TEAM_SETTINGS__SITE_NAME:=Mattermost}
-export SQL_SETTINGS__DRIVER_NAME=postgres
-export SQL_SETTINGS__DATA_SOURCE=${DATABASE_URL}
+[[ ${FILE_SETTINGS__DRIVER_NAME} != "local" ]] || echo "WARNING!!!!! FILES ARE STORED ON DISK AND WILL BE REGULARLY REMOVED, THIS MEANS UPLOADS WILL DISAPEAR!!"
 
 lib/envsubst < config/config-heroku-template.json > config/config-heroku.json
 
