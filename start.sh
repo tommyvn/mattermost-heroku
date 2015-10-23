fd5b81251eb3e527a37939d77c7c6d360b6df48d
Tom van Neerijnen <tom@tomvn.com>
2015-10-23 15:51:33 +0100

    normalise all encryption keys and salts to 32 chars

--- start.sh
+++ start.sh
@@ -2,9 +2,12 @@
 
 [[ ${FILE_SETTINGS__DRIVER_NAME} != "local" ]] || echo "WARNING!!!!! FILES ARE STORED ON DISK AND WILL BE REGULARLY REMOVED, THIS MEANS UPLOADS WILL DISAPEAR!!"
 
-# SqlSettings.AtRestEncryptKey expects 32 characters but Heroku doesn't define how many characters
-#  a secret type generates. This makes sure we've got our 32 characters
+# Encryption keys and salts seem to have a requirement of 32 characters but Heroku doesn't
+#   define how many characters a secret type generates. This normalises to 32 characters.
 SQL_SETTINGS__AT_REST_ENCRYPT_KEY=$(printf "${SQL_SETTINGS__AT_REST_ENCRYPT_KEY}%.0s" {1..32} | cut -c -32)
+FILE_SETTINGS__PUBLIC_LINK_SALT=$(printf "${FILE_SETTINGS__PUBLIC_LINK_SALT}%.0s" {1..32} | cut -c -32)
+EMAIL_SETTINGS__INVITE_SALT=$(printf "${EMAIL_SETTINGS__INVITE_SALT}%.0s" {1..32} | cut -c -32)
+EMAIL_SETTINGS__PASSWORD_RESET_SALT=$(printf "${EMAIL_SETTINGS__PASSWORD_RESET_SALT}%.0s" {1..32} | cut -c -32)
 
 # default s3 stuff to empty if it's not configured
 export FILE_SETTINGS__AMAZON_S3_ACCESS_KEY_ID=${FILE_SETTINGS__AMAZON_S3_ACCESS_KEY_ID:=""}
